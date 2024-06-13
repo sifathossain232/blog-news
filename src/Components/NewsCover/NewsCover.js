f4df2d58876783d49f0d83daeca78deeb1d6be3b
SIFAT HOSSAIN <143956074+sifathossain232@users.noreply.github.com>
2024-06-13 18:18:56 +0600

    add blog news

---
+++ src/Components/NewsCover/NewsCover.js
@@ -0,0 +1,72 @@
+"use client"
+import Image from "next/image";
+import { newsData } from './../../newsData/News';
+import { useEffect, useState } from "react";
+import Link from "next/link";
+const NewsCoverPage = () => {
+    const [data, setData] = useState([]);
+    const [visibleData, setVisibleData] = useState([]);
+    const [itemsToShow, setItemsToShow] = useState(5);
+
+    useEffect(() => {
+        setVisibleData(newsData.slice(0, itemsToShow));
+        setData(newsData);
+    },[itemsToShow]);
+
+    const loadMore = () => {
+        const newItemsToShow = itemsToShow + 5;
+        setItemsToShow(newItemsToShow);
+        setVisibleData(data.slice(0, newItemsToShow));
+    };
+
+
+    return (
+        <div>
+            <div className='w-[1440px] h-[835px] bg-[#FCF7EF]'>
+                <div className='text-center pt-[70px]'>
+                    <h1 className='text-[90px] font-semibold'>Our News</h1>
+                    
+                    <p className='text-xl font-medium mt-[17px]'>Get the latest updates and deeper coffee experience from IMAJI Coffee</p>
+                </div>
+                <div className="flex justify-center mt-10 mb-[30px]">
+                    <Image src='/image/cover.png' alt="Loding...." width={1194} height={406} />
+                </div>
+                <div className="ml-[123px]">
+                    <p className="text-3xl font-medium">Collaboration to Develop Coffee and Beverage Industry Expertise in Indonesia</p>
+                    <div className="flex gap-[10px] mt-[10px]">
+                        <p className="text-xl font-normal">4 Min</p>
+                        <li className="text-xl font-normal">August 19,2024</li>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-20 w-[1224px] mx-auto">
+            
+                    {visibleData.map((news) => (
+                        <Link  key={news.id} href={`/${news.id}`}>
+                        <div className="border-t-[1.5px] border-[#B8B8B8]">
+                            <div className="flex gap-[60px] items-center  pt-5 pb-[50px]">
+                                <Image src={news?.image} width={261} height={205} alt="" />
+                                <div>
+                                    <h1 className="text-[46px] font-medium">{news?.title}</h1>
+                                    <div className="flex gap-[10px]">
+                                        <p className="text-xl font-normal">{news?.time}</p>
+                                        <li className="text-xl font-normal">{news?.date}</li>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        </Link>
+                    ))}
+               
+                <div className="flex justify-center mt-[30px] pb-10">
+                    {/* <button className="border border-[#986B24] py-[14px] px-[24px]">Load More</button> */}
+                    {visibleData.length < data.length && (
+                        <button className="border border-[#986B24] py-[14px] px-[24px]" onClick={loadMore}>Load More</button>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NewsCoverPage;
---
+++ src/app/[ids]/page.js
@@ -0,0 +1,80 @@
+"use client"
+import { newsData } from "@/newsData/News";
+import Image from "next/image";
+import { useParams } from "next/navigation";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+// import './styles.css';
+import { Pagination } from 'swiper/modules';
+
+const NewsDetails = () => {
+    const { id } = useParams();
+    const allData = newsData.map((item) => item)
+    const data = allData.find((items) => items.id !== id)
+    return (
+        <div className="w-[1440px] mx-auto">
+            <h1 className="text-center text-[66px] font-semibold ml-[193px] mr-[263px]">{data?.title}</h1>
+            <p className="text-center">{data?.date}</p>
+            <div className="flex justify-center">
+                <Image className="w-[1320px] h-[521px] my-[70px]" src={data?.image} alt="Loding...." width={1320} height={521} />
+            </div>
+            <div className="px-[60px]">
+                <h3 className="mb-5 text-2xl">{data?.disc1}</h3>
+                <h3 className=" mb-5 text-2xl">{data?.disc2}</h3>
+                <h3 className="mb-5 text-2xl">{data?.disc3}</h3>
+                <h3 className="mb-5 text-2xl">{data?.disc4}</h3>
+                <h3 className="mb-5 text-2xl">{data?.disc5}</h3>
+                <h3 className="mb-5 text-2xl">{data?.disc6}</h3>
+                <h3 className="mb-[134px] text-2xl">{data?.disc7}</h3>
+            </div>
+            <div className="bg-[#FCF7EF] h-[887px] p-[60px]">
+                <div className="border-b-[1.5px] border-[#B8B8B8]">
+                    <h1 className="text-[70px] font-semibold mb-10">Other News</h1>
+                </div>
+                <Swiper
+                    slidesPerView={2}
+                    spaceBetween={30}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper mt-10"
+                >
+                    <SwiperSlide>
+                        <div>
+                            <Image src="/image/img1.png" alt="Loding...." width={648} height={415} />
+                            <h1 className="text-[22px] font-medium mt-5">Collaboration to Develop Coffee and Beverage Industry Expertise in Indonesia</h1>
+                            <div className="flex gap-[10px] mt-[18px]">
+                                <p className="text-xl">4 Min</p>
+                                <li className="text-xl">August 19,2024</li>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div>
+                            <Image src="/image/img2.png" alt="Loding...." width={648} height={415} />
+                            <h1 className="text-[22px] font-medium mt-5">Collaboration to Develop Coffee and Beverage Industry Expertise in Indonesia</h1>
+                            <div className="flex gap-[10px] mt-[18px]">
+                                <p className="text-xl">4 Min</p>
+                                <li className="text-xl">August 19,2024</li>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div>
+                            <Image src="/image/img3.png" alt="Loding...." width={648} height={415} />
+                            <h1 className="text-[22px] font-medium mt-5">Collaboration to Develop Coffee and Beverage Industry Expertise in Indonesia</h1>
+                            <div className="flex gap-[10px] mt-[18px] pb-[]">
+                                <p className="text-xl">4 Min</p>
+                                <li className="text-xl">August 19,2024</li>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                </Swiper>
+            </div>
+        </div>
+    );
+};
+
+export default NewsDetails;
